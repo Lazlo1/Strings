5db8469a4b194c8e8ca99974c1abb19942a234cd
Lazlo1 <lazlo102@gmail.com>
2017-03-26 21:47:12 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
-var animal ='Zielone słonie';
-var bigAnimal = animal.toUpperCase();
-var textCharsAfter = text.replace('Papugi', bigAnimal);
-var newText = textCharsAfter.substr(bigAnimal, 90/2);
-
-	console.log(newText);
-
-
